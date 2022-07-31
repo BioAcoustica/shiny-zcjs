6c85e2c6b70b90b494d6c8336763dbc1f2358e3e
Ed Baker <edwbaker@gmail.com>
2022-07-31 09:53:31 +0100

    Citations.'

--- app.R
+++ app.R
@@ -1,11 +1,12 @@
 library(shiny)
 library(zcjs)
+library(schite)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("zcjs: demo"),
+    titlePanel("Demonstration of zcjs in Shiny"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -18,7 +19,12 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           zcjsOutput("zcjs")
+           zcjsOutput("zcjs"),
+           tabsetPanel(type = "tabs",
+                       tabPanel("How to cite", htmlOutput("citation")),
+                       tabPanel("Data Visualisation", htmlOutput("citevis")),
+                       tabPanel("Misc", htmlOutput("citmisc"))
+           )
         )
     )
 )
@@ -30,6 +36,30 @@ server <- function(input, output) {
     output$zcjs <- renderZcjs({
         zcjs(f, x_compress=input$x_compress)
     })
+    
+    citation <- list(
+      cite_bibentry(
+        bibentry(
+          bibtype="Misc",
+          title="Demonstration of zcjs in Shiny",
+          author="Ed Baker",
+          url="http://shiny.ebaker.me.uk/shiny-zcjs/",
+          year=2022
+        )
+      )
+    )
+    output$citation <- citationTabUI(citation)
+    
+    citvis <- list(
+      cite_r_package("zcjs")
+    )
+    output$citevis <- citationTabUI(citvis)
+    
+    citmisc <- list(
+      cite_r_package("schite"),
+      cite_r_package("shiny")
+    )
+    output$citmisc <- citationTabUI(citmisc)
 }
 
 # Run the application
